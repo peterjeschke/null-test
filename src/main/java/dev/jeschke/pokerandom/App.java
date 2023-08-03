600b8da230bb67e3542b3990c11427c9c4d7527f
Peter-René Jeschke <peter-rene.jeschke@ratepay.com>
2023-08-03 17:19:37 +0200

    Initial commit

--- src/main/java/dev/jeschke/pokerandom/App.java
+++ src/main/java/dev/jeschke/pokerandom/App.java
@@ -1,13 +1,18 @@
 package dev.jeschke.pokerandom;
 
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( final String[] args ) {
+        final MyObject build = MyObject.builder()
+                .nonNullString( "" )
+                .myString( "" )
+                .build();
+        final String nonNullString = build.getNonNullString();
+        System.out.println( nonNullString.toLowerCase() );
+        System.out.println( build.getMyString().toLowerCase() );
     }
 }
--- src/main/java/dev/jeschke/pokerandom/MyObject.java
+++ src/main/java/dev/jeschke/pokerandom/MyObject.java
@@ -1,2 +1,18 @@
-package dev.jeschke.pokerandom;public class MyObject {
+package dev.jeschke.pokerandom;
+
+
+import org.jetbrains.annotations.Nullable;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+
+@Data
+@Builder
+public class MyObject {
+    @Nullable
+    private String myString;
+    @NonNull
+    private String nonNullString;
 }
